d84db9af59e64fd6d1196a72c9fe656c063732a6
karol-roman18 <66563714+karol-roman18@users.noreply.github.com>
2022-11-26 20:00:34 +0100

    Ground_effect_first_order_upwind_discretization
    Files for the ground effect first order upwind spatial discretization scheme.

---
+++ GroundUpwind/postpro.sh
@@ -0,0 +1,25 @@
+extractVal()
+{
+    if [ -f "$2" ]
+    then
+        foamDictionary -entry "$1" -value "$2" | \
+            sed -n '/(/,/)/{ s/[()]//g; /^ *$/d; p}' \
+            > "$3"
+    else
+        # Or some other tag?
+        echo "Not such file: $2" 1>&2
+        echo "0" > "$3"
+    fi
+}
+        timeDir=$(foamListTimes -latestTime)
+
+        # Create datasets for benchmark comparisons
+        extractVal boundaryField.Airfoil.value "$timeDir/Cx" "Cx.$$"
+        extractVal boundaryField.Airfoil.value "$timeDir/wallShearStress" "tau.$$"
+        extractVal boundaryField.Airfoil.value "$timeDir/Cp" "cp.$$"
+        extractVal boundaryField.Airfoil.value "$timeDir/yPlus" "yplus.$$"
+
+        echo "# ccx tau_xx tau_yy tau_zz cp yplus" > profiles.dat
+        paste -d ' ' Cx.$$ tau.$$ cp.$$ yplus.$$ >> profiles.dat
+        rm -f Cx.$$ tau.$$ cp.$$ uplus.$$
+
